23d71807a3d3b77fb2082418df4fa1e693a9fa8f
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-15 18:09:21 +0100

    Update deployment configuration

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,4 +1,13 @@
 terraform {
+  cloud {
+
+    organization = "paty-training"
+
+    workspaces {
+      name = "gh-actions-snowflake-sephora"
+    }
+  }
+
   required_providers {
     snowflake = {
       source = "Snowflake-Labs/snowflake"
