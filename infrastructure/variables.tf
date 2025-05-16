92d42257e4154a15b7e0160cf222ce05461fda63
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-16 18:11:26 +0100

    Add snowflake provider

---
+++ infrastructure/variables.tf
@@ -0,0 +1,21 @@
+variable "organization_name" {
+    type = string
+    description = "Snowflake organization name."
+}
+
+variable "account_name" {
+    type = string
+    description = "Snowflake account name."
+}
+
+variable "username" {
+  type = string
+  description = "Snowflake user name"
+  sensitive = true
+}
+
+variable "private_key_path" {
+  type = string
+  description = "Filepath of private key"
+  sensitive = true
+}
