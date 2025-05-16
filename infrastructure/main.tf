1774201b6547897014839745c7e739c787c1bc5a
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-16 18:11:57 +0100

    Fix formating issues

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 provider "snowflake" {
-    organization_name =  var.organization_name
-    account_name = var.account_name
-    user = var.username
-    authenticator = "SNOWFLAKE_JWT"
-    private_key = file(var.private_key_path)
+  organization_name = var.organization_name
+  account_name      = var.account_name
+  user              = var.username
+  authenticator     = "SNOWFLAKE_JWT"
+  private_key       = file(var.private_key_path)
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,21 +1,21 @@
 variable "organization_name" {
-    type = string
-    description = "Snowflake organization name."
+  type        = string
+  description = "Snowflake organization name."
 }
 
 variable "account_name" {
-    type = string
-    description = "Snowflake account name."
+  type        = string
+  description = "Snowflake account name."
 }
 
 variable "username" {
-  type = string
+  type        = string
   description = "Snowflake user name"
-  sensitive = true
+  sensitive   = true
 }
 
 variable "private_key_path" {
-  type = string
+  type        = string
   description = "Filepath of private key"
-  sensitive = true
+  sensitive   = true
 }
