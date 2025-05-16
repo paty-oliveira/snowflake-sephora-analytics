50f489062b0a30a895a32ebc6dfef279a969c8e0
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-16 18:40:51 +0100

    Create database

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,3 +5,11 @@ provider "snowflake" {
   authenticator     = "SNOWFLAKE_JWT"
   private_key       = file(var.private_key_path)
 }
+
+resource "snowflake_account_role" "engineer_role" {
+  name = "ROLE_ENGINEER"
+}
+
+resource "snowflake_database" "sephora_db" {
+  name = "SEPHORA_ANALYTICS"
+}
