e49363756fc12eeb82fc25e60a5de59eede57f16
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-17 17:43:20 +0100

    Grant privileges to access Sephora database schema

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,11 +5,14 @@ provider "snowflake" {
   password          = var.snowflake_password
 }
 
+
+# Create ENGINEER_ROLE role
 resource "snowflake_account_role" "engineer_role" {
   name = "ROLE_ENGINEER"
 }
 
-resource "snowflake_grant_privileges_to_account_role" "engineer_role_priviliges" {
+# Grant ALL PRIVILEGES to ROLE_ENGINEER role for the Sephora database
+resource "snowflake_grant_privileges_to_account_role" "engineer_role_priviliges_db" {
   account_role_name = snowflake_account_role.engineer_role.name
   all_privileges    = true
   on_account_object {
@@ -18,17 +21,29 @@ resource "snowflake_grant_privileges_to_account_role" "engineer_role_priviliges"
   }
 }
 
+# Grant ALL PRIVILEGES to ROLE_ENGINEER role for all the schemas of Sephora database
+resource "snowflake_grant_privileges_to_account_role" "engineer_role_priviliges_schema" {
+  account_role_name = snowflake_account_role.engineer_role.name
+  all_privileges    = true
+  on_schema {
+    all_schemas_in_database = snowflake_database.sephora_db.name
+  }
+}
+
+# Grant user to ROLE_ENGINEER role
 resource "snowflake_grant_account_role" "engineer_user_grant" {
   role_name = snowflake_account_role.engineer_role.name
   user_name = var.snowflake_user
 }
 
+# Create WAREHOUSE_ENGINEER_S warehouse
 resource "snowflake_warehouse" "engineer_warehouse" {
   name           = "WAREHOUSE_ENGINEER_S"
   warehouse_size = "small"
 }
 
-resource "snowflake_grant_privileges_to_account_role" "warehouse_role_priviliges" {
+# Grant ALL PRIVILEGES to ROLE_ENGINEER role for the WAREHOUSE_ENGINEER_S warehouse
+resource "snowflake_grant_privileges_to_account_role" "warehouse_role_privileges" {
   account_role_name = snowflake_account_role.engineer_role.name
   all_privileges    = true
   on_account_object {
@@ -37,6 +52,7 @@ resource "snowflake_grant_privileges_to_account_role" "warehouse_role_priviliges
   }
 }
 
+# Create Sephora database
 resource "snowflake_database" "sephora_db" {
   name = "SEPHORA_ANALYTICS"
 }
