c07458efd8ff2e48fb909b6573ec637e3eae4d7f
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-21 14:32:02 +0100

    Grant permissions to tables and views in all schemas

--- infrastructure/snowflake/modules/terraform-snowflake-database/main.tf
+++ infrastructure/snowflake/modules/terraform-snowflake-database/main.tf
@@ -2,6 +2,14 @@ resource "snowflake_database" "this" {
   name = var.database_name
 }
 
+resource "snowflake_schema" "schemas" {
+  for_each = var.database_schemas
+
+  name     = each.value
+  database = snowflake_database.this.name
+}
+
+
 resource "snowflake_grant_privileges_to_account_role" "grant_all_privileges_on_database" {
   account_role_name = var.database_role_name
   all_privileges    = true
@@ -19,9 +27,24 @@ resource "snowflake_grant_privileges_to_account_role" "grant_all_privileges_on_a
   }
 }
 
-resource "snowflake_schema" "schemas" {
-  for_each = var.database_schemas
+resource "snowflake_grant_privileges_to_account_role" "grant_select_on_all_tables" {
+  account_role_name = var.database_role_name
+  privileges        = ["SELECT", "INSERT"]
+  on_schema_object {
+    all {
+      object_type_plural = "TABLES"
+      in_database        = snowflake_database.this.name
+    }
+  }
+}
 
-  name     = each.value
-  database = snowflake_database.this.name
+resource "snowflake_grant_privileges_to_account_role" "grant_select_on_all_views" {
+  account_role_name = var.database_role_name
+  privileges        = ["SELECT", "INSERT"]
+  on_schema_object {
+    all {
+      object_type_plural = "VIEWS"
+      in_database        = snowflake_database.this.name
+    }
+  }
 }
