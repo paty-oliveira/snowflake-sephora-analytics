b606b266c804c17c354d3a786ac986a92812f8b0
paty-oliveira <dev.patricia.oliveira@gmail.com>
2025-05-28 18:51:45 +0100

    Enrich reviews_sentiment_analysis data model with product information

--- transformation/models/reporting/reviews_sentiment_analysis.sql
+++ transformation/models/reporting/reviews_sentiment_analysis.sql
@@ -14,7 +14,7 @@ with removed_special_chars as (
     from {{ ref('fct_customer_reviews') }}
 ),
 
-aggreated_reviews as (
+aggregated_reviews as (
     select
         product_id,
         array_agg(review_text) within group (order by submitted_at desc) as all_reviews,
@@ -23,20 +23,40 @@ aggreated_reviews as (
         max(submitted_at) as last_submitted_at
     from removed_special_chars
     group by product_id
+),
+
+enriched_product_information as (
+    select
+        reviews.product_id,
+        products.name as product_name,
+        products.type as product_type,
+        products.category as product_category,
+        products.subcategory as product_sub_category,
+        array_to_string(reviews.all_reviews, '<review>') as all_reviews,
+        reviews.avg_rating,
+        reviews.total_reviews,
+        reviews.last_submitted_at
+    from aggregated_reviews as reviews
+    join {{ ref('dim_product') }} as products
+        on reviews.product_id = products.product_id
 )
 
 select
     product_id,
+    product_name,
+    product_type,
+    product_category,
+    product_sub_category,
     snowflake.cortex.try_complete(
         'claude-3-5-sonnet',
         concat('You are a helpful assistant specialized in analyzing customer feedback. Below is a
         list of customer reviews for a product. Please read all the reviews carefully and provide
         a concise summary that captures the main sentiments, recurring themes (both positive and
         negative), and any common suggestions for improvement. Customer Reviews: ',
-        array_to_string(all_reviews, '<review>'))
+        all_reviews)
     ) as sentiment_summary,
     avg_rating,
     total_reviews,
     last_submitted_at,
     to_timestamp_ntz(current_timestamp) as created_at
-from aggreated_reviews
+from enriched_product_information
